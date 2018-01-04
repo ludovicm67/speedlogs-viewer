e4bc246395879c04938eaf94ff3768e8e0b8b215
ludovicm67 <ludovicmuller1@gmail.com>
2018-01-04 15:14:18 +0100

    beautiful chart

--- index.php
+++ index.php
@@ -16,12 +16,25 @@ if (isset($conf['path_to_logfile'])
 } else {
   $file_name = NULL;
 }
+$display_chart = false;
 ?>
 <!DOCTYPE html>
 <html>
 <head>
   <meta charset="utf-8">
   <title>Speedlogs viewer</title>
+  <style>
+    * {
+      box-sizing: border-box;
+      margin: 0;
+      padding: 0;
+    }
+    body {
+      font-family: sans-serif;
+      font-size: 16px;
+      min-width: 800px;
+    }
+  </style>
 </head>
 <body>
   <div>
@@ -46,26 +59,90 @@ if (isset($conf['path_to_logfile'])
         The file exists, but it is empty or bad-formed.
       </p>
     <?php else: ?>
-      <ul>
       <?php
         if ($max_items > 0) {
           $file_parsed = array_slice($file_parsed, -$max_items);
         }
+        $display_chart = true;
       ?>
-
-      <?php foreach ($file_parsed as $item): ?>
-        <li>
-          <ul>
-            <li>Ping: <?= $item['ping']; ?> ms</li>
-            <li>Download: <?= $item['down']; ?> Mbps</li>
-            <li>Upload: <?= $item['up']; ?> Mbps</li>
-            <li>Time: <?= $item['time']; ?></li>
-          </ul>
-        </li>
-      <?php endforeach; ?>
-      </ul>
+      <canvas id="speedlogs-chart"></canvas>
     <?php endif; ?>
   <?php } ?>
   </div>
+<?php if ($display_chart): ?>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
+  <script>
+    var ctx = document.getElementById('speedlogs-chart').getContext('2d');
+    var chart = new Chart(ctx, {
+      // The type of chart we want to create
+      type: 'line',
+
+      // The data for our dataset
+      data: {
+        labels: [
+          '<?= implode("', '", array_column($file_parsed, 'time')); ?>'
+        ],
+        datasets: [{
+          label: "Ping (ms)",
+          fill: false,
+          backgroundColor: 'rgb(255, 159, 64)',
+          borderColor: 'rgb(255, 159, 64)',
+          data: [
+            <?= implode(", ", array_column($file_parsed, 'ping')); ?>
+          ],
+          yAxisID: 'y-ms',
+        }, {
+          label: "Download (Mbps)",
+          fill: false,
+          backgroundColor: 'rgb(54, 162, 235)',
+          borderColor: 'rgb(54, 162, 235)',
+          data: [
+            <?= implode(", ", array_column($file_parsed, 'down')); ?>
+          ],
+          yAxisID: 'y-mbps',
+        }, {
+          label: "Upload (Mbps)",
+          fill: false,
+          backgroundColor: 'rgb(75, 192, 192)',
+          borderColor: 'rgb(75, 192, 192)',
+          data: [
+            <?= implode(", ", array_column($file_parsed, 'up')); ?>
+          ],
+          yAxisID: 'y-mbps',
+        }]
+      },
+
+      // Configuration options go here
+      options: {
+        responsive: true,
+        title: {
+          display: true,
+          text: 'Speedlogs'
+        }, scales: {
+          yAxes: [{
+            type: 'linear',
+            display: true,
+            position: 'left',
+            ticks: {
+              beginAtZero: true,
+            },
+            id: 'y-mbps'
+          }, {
+            type: 'linear',
+            display: true,
+            position: 'right',
+            ticks: {
+              min: -1,
+            },
+            id: 'y-ms',
+            gridLines: {
+              drawOnChartArea: false
+            }
+          }]
+        }
+      }
+    });
+  </script>
+<?php endif; ?>
 </body>
 </html>
