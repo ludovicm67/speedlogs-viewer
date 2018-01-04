c212d73962704ac3e2561a74531547ad1288b917
ludovicm67 <ludovicmuller1@gmail.com>
2018-01-04 11:47:28 +0100

    base structure

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+// get configuration
+$conf = parse_ini_file("config.ini");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Speedlogs viewer</title>
+</head>
+<body>
+  <div>
+    Here will be the place of a nice graph!
+  </div>
+</body>
+</html>
