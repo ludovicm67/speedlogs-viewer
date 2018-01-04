a0c2e4f23112357034616588c64c1f988e810451
ludovicm67 <ludovicmuller1@gmail.com>
2018-01-04 12:11:19 +0100

    check existance of the file specified in the configuration file

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <?php
 // get configuration
 $conf = parse_ini_file("config.ini");
+if (isset($conf['path_to_logfile'])
+  && file_exists($conf['path_to_logfile'])
+  && is_readable($conf['path_to_logfile'])) {
+  $file_name = $conf['path_to_logfile'];
+} else {
+  $file_name = false;
+}
 ?>
 <!DOCTYPE html>
 <html>
@@ -8,8 +15,21 @@ $conf = parse_ini_file("config.ini");
   <title>Speedlogs viewer</title>
 </head>
 <body>
+<?php if ($file_name === false): ?>
+  <div>
+    <h1>No logs to display for the moment...</h1>
+    <h2>The log file is not available for the moment.</h2>
+    <p>
+      We aren't able to read it.
+      If the file specified in the configuration file really exists,
+      please check the permissions.
+    </p>
+  </div>
+<?php else: ?>
   <div>
     Here will be the place of a nice graph!
+    <?= $file_name; ?>
   </div>
+<?php endif; ?>
 </body>
 </html>
